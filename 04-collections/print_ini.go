6258f46f894d81cdb4159b0ca495f3ccb2a17fd3
dickeyxxx <jeff@dickeyxxx.com>
2014-07-26 03:47:33 -0700

    use slice to generate ini output

--- 04-collections/print_ini.go
+++ 04-collections/print_ini.go
@@ -3,30 +3,33 @@ package strings
 import (
 	"fmt"
 	"sort"
+	"strings"
 )
 
 // Given an object representing an ini file, returns a string for saving to a .ini file.
-func PrintIni(doc map[string]map[string]string) (ini string) {
+func PrintIni(doc map[string]map[string]string) string {
+	lines := []string{}
 	var groups []string
 	for group, _ := range doc {
 		groups = append(groups, group)
 	}
 	sort.Strings(groups)
 	for _, group := range groups {
-		ini += fmt.Sprintf("[%s]\n", group)
-		ini += printProperties(doc[group])
+		lines = append(lines, fmt.Sprintf("[%s]", group))
+		lines = append(lines, printProperties(doc[group])...)
 	}
-	return
+	lines = append(lines, "")
+	return strings.Join(lines, "\n")
 }
 
-func printProperties(properties map[string]string) (ini string) {
+func printProperties(properties map[string]string) (lines []string) {
 	var names []string
 	for name := range properties {
 		names = append(names, name)
 	}
 	sort.Strings(names)
 	for _, name := range names {
-		ini += fmt.Sprintf("%s=%s\n", name, properties[name])
+		lines = append(lines, fmt.Sprintf("%s=%s", name, properties[name]))
 	}
 	return
 }
