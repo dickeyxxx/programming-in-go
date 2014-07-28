519527fb7e3b46fa1bab6928514b87771118c8b3
dickeyxxx <jeff@dickeyxxx.com>
2014-07-27 18:32:23 -0700

    cleaned up test cases

--- 05-procedural/common_prefix_test.go
+++ 05-procedural/common_prefix_test.go
@@ -7,81 +7,47 @@ import (
 )
 
 func TestCommonPrefix(t *testing.T) {
-	Convey("with no strings", t, func() {
-		strings := []string{}
-		result := CommonPrefix(strings)
-		So(result, ShouldEqual, "")
-	})
+	Convey("with test data", t, func() {
+		tests := []struct {
+			prefix string
+			paths  []string
+		}{
+			{"", []string{}},
+			{"/home/user/goeg", []string{"/home/user/goeg", "/home/user/goeg/prefix", "/home/user/goeg/prefix/extra"}},
+			{"/home/user/", []string{"/home/user/goeg", "/home/user/goeg/prefix", "/home/user/prefix/extra"}},
+			{"/pecan/π/", []string{"/pecan/π/goeg", "/pecan/π/goeg/prefix", "/pecan/π/prefix/extra"}},
+			{"/pecan/π/circle", []string{"/pecan/π/circle", "/pecan/π/circle/prefix", "/pecan/π/circle/prefix/extra"}},
+			{"/home/user", []string{"/home/user/goeg", "/home/users/goeg", "/home/userspace/goeg"}},
+			{"/", []string{"/home/user/goeg", "/tmp/user", "/var/log"}},
+			{"/home/", []string{"/home/mark/goeg", "/home/user/goeg"}},
+			{"", []string{"home/user/goeg", "/tmp/user", "/var/log"}},
+		}
 
-	Convey("with unlike strings", t, func() {
-		strings := []string{"foo", "bar"}
-		result := CommonPrefix(strings)
-		So(result, ShouldEqual, "")
-	})
-
-	Convey("with one string", t, func() {
-		strings := []string{"foobar"}
-		result := CommonPrefix(strings)
-		So(result, ShouldEqual, "foobar")
-	})
-
-	Convey("with same strings", t, func() {
-		strings := []string{"foobar", "foobar"}
-		result := CommonPrefix(strings)
-		So(result, ShouldEqual, "foobar")
-	})
-
-	Convey("with common prefixes", t, func() {
-		strings := []string{"foobar", "foobaz", "foob", "fooer"}
-		result := CommonPrefix(strings)
-		So(result, ShouldEqual, "foo")
-	})
-
-	Convey("with unicode", t, func() {
-		strings := []string{"foΩobar", "foΩobaz", "foΩob", "foΩoer"}
-		result := CommonPrefix(strings)
-		So(result, ShouldEqual, "foΩo")
+		for _, test := range tests {
+			So(CommonPrefix(test.paths), ShouldEqual, test.prefix)
+		}
 	})
 }
 
 func TestPathPrefix(t *testing.T) {
-	Convey("with no strings", t, func() {
-		strings := []string{}
-		result := CommonPathPrefix(strings)
-		So(result, ShouldEqual, "")
-	})
-
-	Convey("with 3 paths", t, func() {
-		strings := []string{"/foo/bar/boy", "/foo/bar/baz", "/foo/bar/zaz"}
-		result := CommonPathPrefix(strings)
-		So(result, ShouldEqual, "/foo/bar")
-	})
-
 	Convey("with test data", t, func() {
-		paths := [][]string{
-			{"/home/user/goeg", "/home/user/goeg/prefix", "/home/user/goeg/prefix/extra"},
-			{"/home/user/goeg", "/home/user/goeg/prefix", "/home/user/prefix/extra"},
-			{"/pecan/π/goeg", "/pecan/π/goeg/prefix", "/pecan/π/prefix/extra"},
-			{"/pecan/π/circle", "/pecan/π/circle/prefix", "/pecan/π/circle/prefix/extra"},
-			{"/home/user/goeg", "/home/users/goeg", "/home/userspace/goeg"},
-			{"/home/user/goeg", "/tmp/user", "/var/log"},
-			{"/home/mark/goeg", "/home/user/goeg"},
-			{"home/user/goeg", "/tmp/user", "/var/log"},
+		tests := []struct {
+			prefix string
+			paths  []string
+		}{
+			{"", []string{}},
+			{"/home/user/goeg", []string{"/home/user/goeg", "/home/user/goeg/prefix", "/home/user/goeg/prefix/extra"}},
+			{"/home/user", []string{"/home/user/goeg", "/home/user/goeg/prefix", "/home/user/prefix/extra"}},
+			{"/pecan/π", []string{"/pecan/π/goeg", "/pecan/π/goeg/prefix", "/pecan/π/prefix/extra"}},
+			{"/pecan/π/circle", []string{"/pecan/π/circle", "/pecan/π/circle/prefix", "/pecan/π/circle/prefix/extra"}},
+			{"/home", []string{"/home/user/goeg", "/home/users/goeg", "/home/userspace/goeg"}},
+			{"", []string{"/home/user/goeg", "/tmp/user", "/var/log"}},
+			{"/home", []string{"/home/mark/goeg", "/home/user/goeg"}},
+			{"", []string{"home/user/goeg", "/tmp/user", "/var/log"}},
 		}
 
-		expectations := []string{
-			"/home/user/goeg",
-			"/home/user",
-			"/pecan/π",
-			"/pecan/π/circle",
-			"/home",
-			"",
-			"/home",
-			"",
-		}
-
-		for i := range paths {
-			So(CommonPathPrefix(paths[i]), ShouldEqual, expectations[i])
+		for _, test := range tests {
+			So(CommonPathPrefix(test.paths), ShouldEqual, test.prefix)
 		}
 	})
 }
